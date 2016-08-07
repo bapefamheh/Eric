e92901de2cb53c661bc1741788d72388ce01fa21
Doctor <Doctor@THEBEASTBELOW>
2016-08-07 04:09:25 -0400

    Initial Project Load

---
+++ Dynamic Project scheduling using MVC Angular JS/C#/ShanuAngularProjectSchedule/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectScheduler.Startup))]
+namespace ProjectScheduler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
